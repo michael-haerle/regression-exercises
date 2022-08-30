8915b0b3efa705823e032ce68c79f980f43475cc
Michael Haerle <michael.haerle01@gmail.com>
2022-08-30 15:37:14 -0500

    updated file

--- wrangle.py
+++ wrangle.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+from sklearn.model_selection import train_test_split
 
 import env
 
@@ -28,7 +29,46 @@ def get_zillow_data():
         # Return the dataframe to the calling code
         return df 
 
-def wrangle_zillow(zillow_df):
+
+def remove_outliers(df, k, col_list):
+    ''' remove outliers from a list of columns in a dataframe 
+        and return that dataframe
+    '''
+    
+    for col in col_list:
+
+        q1, q3 = df[col].quantile([.25, .75])  # get quartiles
+        
+        iqr = q3 - q1   # calculate interquartile range
+        
+        upper_bound = q3 + k * iqr   # get upper bound
+        lower_bound = q1 - k * iqr   # get lower bound
+
+        # return dataframe without outliers
+        
+        df = df[(df[col] > lower_bound) & (df[col] < upper_bound)]
+        
+    return df
+
+
+def prepare_zillow(df):
+    # renaming columns
+    df = df.rename(columns = {'bedroomcnt':'bedrooms', 
+                              'bathroomcnt':'bathrooms', 
+                              'calculatedfinishedsquarefeet':'square_feet',
+                              'taxvaluedollarcnt':'tax_value', 
+                              'yearbuilt':'year_built',})
+    # removing outliers
+    df = remove_outliers(df, 1.5, ['square_feet', 'tax_value', 'taxamount'])
     # dropped the rows with null values
-    zillow_df = zillow_df.dropna()
-    return zillow_df
+    df = df.dropna()
+    # train/validate/test split
+    train_validate, test = train_test_split(df, test_size=.2, random_state=123)
+    train, validate = train_test_split(train_validate, test_size=.3, random_state=123)
+    return train, validate, test 
+
+def wrangle_zillow():
+    '''Acquire and prepare data from Zillow database for explore'''
+    train, validate, test = prepare_zillow(get_zillow_data())
+    
+    return train, validate, test
