b825068c9b324979e273d3c94d2e9c578a8f1061
Michael Haerle <michael.haerle01@gmail.com>
2022-09-01 10:39:20 -0500

    updated file

--- wrangle.py
+++ wrangle.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 from sklearn.model_selection import train_test_split
+import sklearn.preprocessing
 
 import env
 
@@ -8,7 +9,7 @@ def get_connection(db, user=env.user, host=env.host, password=env.password):
     return f'mysql+pymysql://{user}:{password}@{host}/{db}'
 
 def new_zillow_data():
-    return pd.read_sql('SELECT bedroomcnt, bathroomcnt, calculatedfinishedsquarefeet, taxvaluedollarcnt, yearbuilt, taxamount, fips FROM properties_2017 JOIN propertylandusetype USING(propertylandusetypeid) WHERE propertylandusedesc LIKE "Single Family Residential" ;', get_connection('zillow'))
+    return pd.read_sql('SELECT bedroomcnt, bathroomcnt, calculatedfinishedsquarefeet, taxvaluedollarcnt, yearbuilt, lotsizesquarefeet, fips, regionidzip, logerror, transactiondate FROM properties_2017 JOIN propertylandusetype USING(propertylandusetypeid) JOIN predictions_2017 USING(parcelid) WHERE propertylandusedesc LIKE "Single Family Residential" ;', get_connection('zillow'))
 
 def get_zillow_data():
     filename = "zillow.csv"
@@ -50,6 +51,43 @@ def remove_outliers(df, k, col_list):
         
     return df
 
+#defining function filter 
+def filter(x):
+    if x > 1850 and x < 1860:
+        return '1850s'
+    if x > 1859 and x < 1870:
+        return '1860s'
+    if x > 1869 and x < 1880:
+        return '1870s'
+    if x > 1879 and x < 1890:
+        return '1880s'
+    if x > 1889 and x < 1900:
+        return '1890s'
+    if x > 1899 and x < 1910:
+        return '1900s'
+    if x > 1909 and x < 1920:
+        return '1910s'
+    if x > 1919 and x < 1930:
+        return '1920s'
+    if x > 1929 and x < 1940:
+        return '1930s'
+    if x > 1939 and x < 1950:
+        return '1940s'
+    if x > 1949 and x < 1960:
+        return '1950s'
+    if x > 1959 and x < 1970:
+        return '1960s'
+    if x > 1969 and x < 1980:
+        return '1970s'
+    if x > 1979 and x < 1990:
+        return '1980s'
+    if x > 1989 and x < 2000:
+        return '1990s'
+    if x > 1999 and x < 2010:
+        return '2000s'
+    if x > 2009:
+        return '2010s'
+
 
 def prepare_zillow(df):
     # renaming columns
@@ -57,15 +95,56 @@ def prepare_zillow(df):
                               'bathroomcnt':'bathrooms', 
                               'calculatedfinishedsquarefeet':'square_feet',
                               'taxvaluedollarcnt':'tax_value', 
-                              'yearbuilt':'year_built',})
+                              'yearbuilt':'year_built',
+                              'regionidzip':'region_zip',
+                              'lotsizesquarefeet':'lot_square_feet',
+                              'transactiondate':'transaction_date'})
     # removing outliers
-    df = remove_outliers(df, 1.5, ['square_feet', 'tax_value', 'taxamount'])
+    df = remove_outliers(df, 1.5, ['square_feet', 'tax_value', 'lot_square_feet'])
     # dropped the rows with null values
     df = df.dropna()
+    # manualy removing more outliers
+    df.drop(df[df.bedrooms == 0].index, inplace=True)
+    df.drop(df[df.bathrooms < 1].index, inplace=True)
+    df.drop(df[df.bedrooms > 10].index, inplace=True)
+    df.drop(df[df.bathrooms > 10].index, inplace=True)
+    df.drop(df[df.year_built < 1850].index, inplace=True)
+    #applying the filter function to 'year_built' column 
+    df['decade'] = df['year_built'].apply(filter)
     # train/validate/test split
     train_validate, test = train_test_split(df, test_size=.2, random_state=123)
     train, validate = train_test_split(train_validate, test_size=.3, random_state=123)
     return train, validate, test 
+
+
+def scale_data(train, 
+               validate, 
+               test, 
+               cols = ['bedrooms', 'bathrooms', 'square_feet', 'tax_value', 'lot_square_feet']):
+    '''
+    Scales the 3 data splits. 
+    Takes in train, validate, and test data splits and returns their scaled counterparts.
+    If return_scalar is True, the scaler object will be returned as well
+    '''
+    # make copies of our original data so we dont gronk up anything
+    train_scaled = train.copy()
+    validate_scaled = validate.copy()
+    test_scaled = test.copy()
+    #     make the thing
+    scaler = sklearn.preprocessing.MinMaxScaler()
+    #     fit the thing
+    scaler.fit(train[cols])
+    # applying the scaler:
+    train_scaled[cols] = pd.DataFrame(scaler.transform(train[cols]),
+                                                  columns=train[cols].columns.values).set_index([train.index.values])
+                                                  
+    validate_scaled[cols] = pd.DataFrame(scaler.transform(validate[cols]),
+                                                  columns=validate[cols].columns.values).set_index([validate.index.values])
+    
+    test_scaled[cols] = pd.DataFrame(scaler.transform(test[cols]),
+                                                 columns=test[cols].columns.values).set_index([test.index.values])
+    return train_scaled, validate_scaled, test_scaled
+
 
 def wrangle_zillow():
     '''Acquire and prepare data from Zillow database for explore'''
